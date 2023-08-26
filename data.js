1fcc454018317940ea1e34c7116f3416334e1908
amengodk <137509270+amengodk@users.noreply.github.com>
2023-08-26 11:57:52 +0100

    Upload

---
+++ data.js
@@ -0,0 +1,176 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20230623_123352_567",
+      "name": "IMG_20230623_123352_567",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.797827345632628,
+        "pitch": 0.08896760896544187,
+        "fov": 1.405674899209756
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.159975631196362,
+          "pitch": 0.27421842001352204,
+          "rotation": 0.7853981633974483,
+          "target": "1-img_20230623_123348_040"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8984527827672686,
+          "pitch": -0.053916567713578445,
+          "title": "Zona de trabajo en grupo",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.86417968985446,
+          "pitch": -0.218475224650458,
+          "title": "Proyección",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-img_20230623_123348_040",
+      "name": "IMG_20230623_123348_040",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.77778183134404,
+        "pitch": 0.06258412516777412,
+        "fov": 1.405674899209756
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8512271726638687,
+          "pitch": 0.30432184950073804,
+          "rotation": 5.497787143782138,
+          "target": "2-img_20230623_123342_082"
+        },
+        {
+          "yaw": -0.29296341884971966,
+          "pitch": 0.29689062418733414,
+          "rotation": 5.497787143782138,
+          "target": "0-img_20230623_123352_567"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.522438823023716,
+          "pitch": 0.11335246907068353,
+          "title": "Radio escolar",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.2117498847979924,
+          "pitch": -0.2643384483219684,
+          "title": "Croma",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.0270995706871533,
+          "pitch": -0.022470154653978724,
+          "title": "Mesa de trabajo proyectos",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-img_20230623_123342_082",
+      "name": "IMG_20230623_123342_082",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.0499352279678558,
+        "pitch": 0.09111010903968442,
+        "fov": 1.405674899209756
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9112670515188253,
+          "pitch": 0.346214126483229,
+          "rotation": 5.497787143782138,
+          "target": "1-img_20230623_123348_040"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7658081365174683,
+          "pitch": -0.1915310009663962,
+          "title": "Impresión 3D",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.6782999264123646,
+          "pitch": -0.1749241066834557,
+          "title": "Corte Láser",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
